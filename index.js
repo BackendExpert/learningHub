5a51736147e6e9900f63498abdc0931ff910c29e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-06-28 12:26:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const DataRoute = require('./routes/DataRoute')
+const DataRoute = require('./routes/DataRoute');
 
-app.use('/data', DataRoute)
+app.use('/data', DataRoute);
 
 app.use('/', (req, res) => {
-    res.send("Server is Running on 5000")
-})
+    res.send("Server is Running on 5000");
+});
 
-
-module.exports = app;
+app.listen(5000, () => {
+    console.log("Server Started on PORT 5000");
+});
