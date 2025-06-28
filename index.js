fe7dd22857e6a23d800cbc7df879acb53241a3ac
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-06-28 12:31:10 +0530

    Update

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const app = express();
 
 const DataRoute = require('./routes/DataRoute');
 
+app.use(express.json()); // <-- Important to parse JSON body
+
 app.use('/data', DataRoute);
 
 app.use('/', (req, res) => {
     res.send("Server is Running on 5000");
 });
 
+// For Vercel and local, keep listen (Vercel will ignore port)
 app.listen(5000, () => {
     console.log("Server Started on PORT 5000");
 });
