cc98868c5ce856740dfa7694dafd4ebc4faa2807
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-06-28 12:39:16 +0530

    Update DataController.js

--- controllers/DataController.js
+++ controllers/DataController.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
-const DataModel = require('../models/LearningData'); 
+const DataModel = require('../models/LearningData');
 
 const DataController = {
     add_data: async (req, res) => {
@@ -16,7 +16,7 @@ const DataController = {
 
             const exists = await DataModel.findById({ id: dataId });
             if (exists) {
-                return res.status(400).json({ message: `Unexpected ID collision. Please try again.` });
+                return res.json({ message: `Unexpected ID collision. Please try again.` });
             }
 
             const newData = await DataModel.create({
@@ -28,27 +28,26 @@ const DataController = {
                 link
             });
 
-            return res.status(201).json({
+            return res.json({
                 message: 'Data added successfully',
                 data: newData
             });
         } catch (err) {
             console.error(err);
-            return res.status(500).json({
+            return res.json({
                 message: 'Internal Server Error',
                 error: err.message
             });
         }
-    }, 
+    },
 
-    get_alldata: async(req, res) => {
-        try{
-            const alldata = await DataModel.findAll()
-
-            return res.json({ Result: alldata })
-        }
-        catch(err){
-            console.log(err)
+    get_alldata: async (req, res) => {
+        try {
+            const alldata = await DataModel.findAll();
+            return res.json({ Result: alldata });
+        } catch (err) {
+            console.log(err);
+            return res.json({ message: 'Internal Server Error', error: err.message });
         }
     }
 };
